116aaedbc199df70eaee8d5f64d4066ae3af03ee
Tran Phuc Vinh <vinhtranphuc@gmail.com>
2020-10-17 16:24:31 +0700

    add cors filter

--- src/main/java/com/book/BookManagementApplication.java
+++ src/main/java/com/book/BookManagementApplication.java
@@ -1,5 +1,6 @@
 package com.book;
 
+import java.util.Collections;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.book.security.AppProperties;
@@ -32,5 +39,19 @@ public class BookManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookManagementApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.setAllowedOrigins(Collections.singletonList("*"));
+	    config.setAllowedMethods(Collections.singletonList("*"));
+	    config.setAllowedHeaders(Collections.singletonList("*"));
+	    source.registerCorsConfiguration("/**", config);
+	    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	    return bean;
 	}
 }
